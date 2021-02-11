5caf61652f0b1de0503978a547d2cb7cb1231d44
Possawat Tanam <possawat.tanam@mail.ac.th>
2021-02-12 01:18:24 +0700

    [add] add photo to folder images

--- INT203_62130500064_groupwork_3/main.js
+++ INT203_62130500064_groupwork_3/main.js
@@ -1,8 +1,78 @@
 const app = {
   data() {
     return {
-      msg: 'Hello, Vue3'
+      Hokkaido: [{
+          name: 'Fort Goryokaku,Hakodate',
+          url: './images/1.jpg',
+          Like: false
+        },
+        {
+          name: 'Mount Hakodate,Hakodate',
+          url: './images/2.jpg',
+          Like: false
+        },
+        {
+          name: 'Otaru Canal Area,Otaru',
+          url: './images/3.jpg',
+          Like: false
+        },
+        {
+          name: 'Furano Flower Field,Furano',
+          url: './images/4.jpg',
+          Like: false
+        },
+        {
+          name: 'Furano Ski Area,Furano',
+          url: './images/5.jpg',
+          Like: false
+        },
+        {
+          name: 'Abashiri Drift Ice,Abashiri',
+          url: './images/6.jpg',
+          Like: false
+        },
+        {
+          name: 'Jigokudani noboribetsu,Noboribetsu',
+          url: './images/7.jpg',
+          Like: false
+        },
+        {
+          name: 'Aoiike Blue Pond,Biei',
+          url: './images/8.jpg',
+          Like: false
+        },
+        {
+          name: 'Asahiyama Zoo,Asahiyama',
+          url: './images/9.jpg',
+          Like: false
+        },
+        {
+          name: 'Shiroi Koibito,Sapporo',
+          url: './images/10.jpg',
+          Like: false
+        },
+        {
+          name: 'Sapporo TV Tower,Sapporo',
+          url: './images/11.jpg',
+          Like: false
+        },
+        {
+          name: 'Mount Moiwa,Sapporo',
+          url: './images/12.jpg',
+          Like: false
+        },
+      ]
     }​​
-  }​​
+  }​​,
+  methods: {
+    likeIt(index) {
+      this.thaides[index].isLike = !this.thaides[index].isLike
+    }
+  },
+  computed: {
+    countLike() {
+      return this.thaides.filter(t => t.isLike).length
+    }
+  }
 }​​
-Vue.createApp(app).mount('#app')
+var mountedApp = Vue.createApp(app).mount('#app')
